a323e170d5daceda03ae469800da155000db7079
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2021-04-13 12:28:00 +0300

    refactor: Drop Apocryph.PerperUtilities project

--- src/Apocryph.Routing.FunctionApp/RouterInput.cs
+++ src/Apocryph.Routing.FunctionApp/RouterInput.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using Apocryph.Consensus;
 using Microsoft.Azure.WebJobs;
@@ -9,18 +12,17 @@ using Perper.WebJobs.Extensions.Triggers;
 
 namespace Apocryph.Routing.FunctionApp
 {
-    using Apocryph.PerperUtilities;
     public static class RouterInput
     {
         [FunctionName("RouterInput")]
         public static IAsyncEnumerable<Message> RunAsync([PerperTrigger] (IAsyncEnumerable<Message> calls, IAsyncEnumerable<List<Reference>> subscriptions) input, IContext context, IStateEntry<List<Reference>?> lastSubscriptions)
         {
-            var output = ApocryphDataflow.EmptyBlock<Message>();
+            var output = EmptyBlock<Message>();
 
-            var subscriptions = ApocryphDataflow.KeepLastBlock(lastSubscriptions);
+            var subscriptions = KeepLastBlock(lastSubscriptions);
             input.subscriptions.ToDataflow().LinkTo(subscriptions);
 
-            var subscriber = ApocryphDataflow.SubsciberBlock<Reference, Message>(async reference =>
+            var subscriber = SubsciberBlock<Reference, Message>(async reference =>
             {
                 var (targetInput, targetOutput) = await context.CallFunctionAsync<(string, IStream<Message>)>("GetChainInstance", reference.Chain);
 
@@ -33,6 +35,76 @@ namespace Apocryph.Routing.FunctionApp
             input.calls.ToDataflow().LinkTo(output);
 
             return output.ToAsyncEnumerable();
+        }
+
+        private static IPropagatorBlock<T, T> EmptyBlock<T>()
+        {
+            return new BufferBlock<T>(new DataflowBlockOptions { BoundedCapacity = 1 });
+        }
+
+        private static IPropagatorBlock<T, T> KeepLastBlock<T>(IStateEntry<T?> stateEntry) where T : class, new()
+        {
+            var output = new TransformBlock<T, T>(value =>
+            {
+                stateEntry.Value = value;
+                return value;
+            });
+
+            output.Post(stateEntry.Value ?? new T());
+
+            return output;
+        }
+
+        private static IPropagatorBlock<IEnumerable<TKey>, TValue> SubsciberBlock<TKey, TValue>(Func<TKey, Task<ISourceBlock<TValue>>> resolver)
+            where TKey : notnull
+        {
+            var links = new Dictionary<TKey, IDisposable>();
+            var output = EmptyBlock<TValue>();
+            var subscriber = new ActionBlock<IEnumerable<TKey>>(async (subscriptions) =>
+            {
+                var seenSubscriptions = new HashSet<TKey>();
+                foreach (var subscription in subscriptions)
+                {
+                    if (!links.ContainsKey(subscription))
+                    {
+                        var source = await resolver(subscription);
+                        links[subscription] = source.LinkTo(output);
+                    }
+                    seenSubscriptions.Add(subscription);
+                }
+
+                foreach (var (subscription, link) in links)
+                {
+                    if (!seenSubscriptions.Contains(subscription))
+                    {
+                        link.Dispose();
+                    }
+                }
+            });
+
+            return DataflowBlock.Encapsulate(subscriber, output);
+        }
+
+        // FIXME: obsoleted by https://github.com/obecto/perper/commit/c21139721cce283a9544619830b67ca8bb5fbee6
+        private static ISourceBlock<T> ToDataflow<T>(this IAsyncEnumerable<T> enumerable, CancellationToken cancellationToken = default)
+        {
+            var block = new BufferBlock<T>(new DataflowBlockOptions { CancellationToken = cancellationToken, BoundedCapacity = 1 });
+
+            async Task helper()
+            {
+                await foreach (var item in enumerable.WithCancellation(cancellationToken))
+                {
+                    await block.SendAsync(item);
+                }
+            }
+
+            helper().ContinueWith(completedTask =>
+            {
+                if (completedTask.Status == TaskStatus.Faulted) ((IDataflowBlock)block).Fault(completedTask.Exception!);
+                else if (completedTask.Status == TaskStatus.RanToCompletion) block.Complete();
+            });
+
+            return block;
         }
     }
 }
