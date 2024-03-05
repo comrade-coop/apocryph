966ed9b80c52676c2e3cf9cd4d4fdadad8a914bb
Houadef Salih <salih.houadef@gmail.com>
2024-03-05 15:41:19 +0100

    fix: explicitly add namespaces to lifecycle cluster
    helmfile template command does not add the namespaces in te
    manifest
    
    adjust devcserver to new changes
    
    also steps to the qemu script similar to minikube test

--- test/e2e/constellation/qemu/run-test.sh
+++ test/e2e/constellation/qemu/run-test.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+set -e
+
+chart_path="$1"
+constellation_path="$2"
+workspace_path="$3"
+current_dir=$(pwd)
+
+if [ -n "$4" ]; then
+  STEP=${4:-1}
+  eval "set -v; $(sed -n "/## $STEP: /{:a;n;p;ba};" $0)"
+  exit
+fi
 
 # Check the number of arguments
 if [ "$#" -lt 3 ]; then
@@ -6,30 +18,24 @@ if [ "$#" -lt 3 ]; then
     exit 1
 fi
 
-current_dir=$(pwd)
+## 0: Generate helm template and inject it into constellation base image
+helmfile template -f "$chart_path" > "$constellation_path/image/base/mkosi.skeleton/usr/lib/helmfile-template"
 
-# Generate helm template and inject it into constellation base image
-helmfile template -f $1 > $2/image/base/mkosi.skeleton/usr/lib/helmfile-template
-
-# cleanup leftover
-cd $3
-constellation terminate
-sudo rm -r $3 2>/dev/null
-mkdir $3
-
+## 1: Build modified image
 cd $current_dir
-cd $2
-# Get go dependecies
+cd "$constellation_path"
 bazel run //:tidy
 
-# Build the image
+## 1.1: Build the image
+cd $current_dir
+cd "$constellation_path"
 bazel build //image/system:qemu_stable
 
-# Get the new image measurements
+## 1.2: Get the new image measurements
 link=$(readlink -f bazel-out/k8-opt/bin/image/system/qemu_qemu-vtpm_stable) 
-output=$(bazel run --run_under="sudo -E" //image/measured-boot/cmd $link/constellation.raw $3/custom-measurements.json 2>&1)
+output=$(bazel run --run_under="sudo -E" //image/measured-boot/cmd $link/constellation.raw "$workspace_path/custom-measurements.json" 2>&1)
 
-# Extract relevant PCR values
+## 1.3: Extract relevant PCR values
 PCR4=$(echo "$output" | sed -n '/PCR\[ *4\]/ s/.*: \(.*\)/\1/p')
 PCR9=$(echo "$output" | sed -n '/PCR\[ *9\]/ s/.*: \(.*\)/\1/p')
 PCR11=$(echo "$output" | sed -n '/PCR\[ *11\]/ s/.*: \(.*\)/\1/p')
@@ -38,7 +44,16 @@ echo "PCR4:  $PCR4"
 echo "PCR9:  $PCR9"
 echo "PCR11: $PCR11"
 
-cd $3
+## 2: terminate clutser
+cd "$workspace_path"
+constellation terminate
+sudo rm -r "$workspace_path" 2>/dev/null
+mkdir "$workspace_path"
+
+
+## 3: create,configure, run workspace
+cd "$current_dir"
+cd "$workspace_path"
 
 constellation config generate qemu
 
@@ -55,5 +70,5 @@ version=$(echo "$output" | grep -oP 'Version:\s+\K\S+' | head -n 1)
 # Copy the image & rename it to the current constellation version to bypass downloading upstream image
 cp $link/constellation.raw "$version.raw" 
 
-# Start the constellation cluster
+## 3.1 Start the constellation cluster
 constellation apply -y
