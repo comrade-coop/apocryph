8efbb97de1d9e4dae766b5a79cdcf0908f4137a5
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2025-04-03 02:23:03 +0300

    fix: default to Base and not Base Sepolia

--- frontend/src/wallet.tsx
+++ frontend/src/wallet.tsx
@@ -1,9 +1,9 @@
 import { Chain, defineChain } from 'viem'
 import { createConfig, http } from 'wagmi'
-import { baseSepolia } from 'wagmi/chains'
+import { base } from 'wagmi/chains'
 import { metaMask } from 'wagmi/connectors'
 
-const chain = import.meta.env.VITE_CHAIN_CONFIG ? defineChain(JSON.parse(import.meta.env.VITE_CHAIN_CONFIG) as Chain) : baseSepolia
+const chain = import.meta.env.VITE_CHAIN_CONFIG ? defineChain(JSON.parse(import.meta.env.VITE_CHAIN_CONFIG) as Chain) : base
  
 export const config = createConfig({
   chains: [chain],
